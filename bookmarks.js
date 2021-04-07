f7fbbbf774b62c26a0466f8ae16d845b828a3a31
Abhimanyu8 <luitindia@gmail.com>
2021-04-07 12:19:07 +0530

    Customised to fit my needs

--- bookmarks.js
+++ bookmarks.js
@@ -5,15 +5,17 @@ const bookmarks = [
     links: [
       { name: "Inbox", url: "https://inbox.google.com" },
       { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
+      { name: "Wikipedia", url: "https://en.wikipedia.org" },
+      { name: "Classroom", url: "https://classroom.google.com" },
     ],
   },
   {
     title: "Media",
     links: [
       { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
-      { name: "Crunchyroll", url: "https://crunchyroll.com" },
+      { name: "Spotify", url: "https://open.spotify.com" },
+      { name: "Anime", url: "https://animepahe.com/" },
+      { name: "Fitgirl", url: "https://fitgirl-repacks.site/" },
       {
         name: "Amazon Prime",
         url: "https://www.amazon.com/Amazon-Video",
@@ -23,17 +25,32 @@ const bookmarks = [
   {
     title: "Reddit",
     links: [
+      { name: "Feed", url: "https://reddit.com" },
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
       { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
-      {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
-      },
+      {name: "/r/pcmasterrace",url: "https://reddit.com/r/pcmasterrace",},
       { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
       {
         name: "/r/battlestations",
         url: "https://reddit.com/r/battlestations",
       },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+      { name: "/r/news", url: "https://reddit.com/r/news" },
+    ],
+  },
+  {
+    title: "4chan",
+    links: [
+      { name: "/g/", url: "https://boards.4channel.org/g/" },
+      {
+        name: "/wg/",
+        url: "https://boards.4chan.org/wg/",
+      },
+      { name: "/a/", url: "https://boards.4channel.org/a/" },
+      {
+        name: "/bant/",
+        url: "https://boards.4chan.org/bant/",
+      },
+      { name: "/o/", url: "https://boards.4channel.org/o/" },
       { name: "/r/news", url: "https://reddit.com/r/news" },
     ],
   },
@@ -42,6 +59,9 @@ const bookmarks = [
     links: [
       { name: "Twitter", url: "https://twitter.com" },
       { name: "Facebook", url: "https://facebook.com" },
+      { name: "Whatsapp", url: "https://web.whatsapp.com" },
     ],
   },
 ];
+
+ 
